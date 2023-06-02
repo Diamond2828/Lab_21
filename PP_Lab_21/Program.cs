4ef97bc65dbeedf6c0f617e4754be775ef4fc5f9
Ameer <vbvbyhhhgh@gmail.com>
2023-06-02 22:42:41 +0300

    Awd

---
+++ PP_Lab_21/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP_Lab_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите а");
+            int a= int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите а");
+            int b= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите действие:\n1. Складывание\n2. Вычитание\n3. Умножение\n Деление");
+            int select=int.Parse(Console.ReadLine());
+            switch (select) 
+            {
+                case 1:
+                    
+                    break;
+
+                case 2:
+                    
+                    break;
+                case 3:
+
+                    break;
+
+                case 4:
+
+                    break;
+            }
+        }
+    }
+}
